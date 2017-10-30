892eb674bdb8472fdeb7fa98e95f51d4a59df733
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-30 20:28:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ class Song
     self.all.sort_by{|song| song.name}
   end
 
-  def new_from_filename(filename)
+  def self.new_from_filename(filename)
     filename_arr = filename.split(/[-\.]/)
 
   end
